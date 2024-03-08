7d4d553b9ae08a945c1568db15f0db94127e2197
VANSH SHARMA <sharmavansh0512@gmail.com>
2024-03-09 01:08:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// // Get the video
+// var video = document.getElementById("myVideo");
+
+// // Get the button
+// var btn = document.getElementById("myBtn");
+
+// // Pause and play the video, and change the button text
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "Pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "Play";
+//   }
+// }
+
+// /* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+// function myFunction() {
+//     var x = document.getElementById("myNavbar");
+//     if (x.className === "navbar") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "navbar";
+//     }
+//   }
